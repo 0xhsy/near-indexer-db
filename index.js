3bac64b93a54f889dea15fbe26bf770e7bce0bb0
0xhsy <sooyoung.hyun@dsrvlabs.com>
2022-07-14 16:16:15 +0900

    add prefix to implicitID

--- index.js
+++ index.js
@@ -16,12 +16,27 @@ app.get('/', async (req, res) => {
 app.get('/accounts', async function (req, res) {
     const address = req.query['address'];
     const connection = [
-    // TODO: connection 저장해놓고 불러와서 사용하기
         'postgres://public_readonly:nearprotocol@mainnet.db.explorer.indexer.near.dev/mainnet_explorer',
         'postgres://public_readonly:nearprotocol@testnet.db.explorer.indexer.near.dev/testnet_explorer',
     ];
 
-    const accountsIds = await Promise.all(connection.map(async (connectionString) => {
+    const addPrefix = (rows, net) => {
+        if(rows.length){
+            rows.map((row)=>{
+                if(hasAccountId(row['account_id'])){
+                    return row['account_id'];
+                }else{
+                    return row['account_id'] = net + ':' + row['account_id'];
+                }
+            });
+        }
+    };
+  
+    const hasAccountId = (address) => {
+        return address.split('.').pop() == 'near' || address.split('.').pop() == 'testnet';
+    };
+
+    const accountsIds = await Promise.all(connection.map(async (connectionString, idx) => {
 
         const { rows } = await new Pool({
             connectionString,
@@ -33,6 +48,12 @@ app.get('/accounts', async function (req, res) {
               AND accounts.deleted_by_receipt_id IS NULL
               AND access_keys.deleted_by_receipt_id IS NULL
         `, [address]);
+
+        if(idx){
+            addPrefix(rows, 'testnet');
+        }else{
+            addPrefix(rows, 'mainnet');
+        }
 
         return rows;
 
